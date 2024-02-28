9dd83f94d53b9040678910ace70b4249fd1f6c11
Zero2220 <153716590+Zero2220@users.noreply.github.com>
2024-02-28 18:01:22 +0400

    Add files via upload

---
+++ CourseDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    internal class CourseDbContext:DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-J30FIEB\\SQLEXPRESS;Database=BrandDb;Trusted_connection=True");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using task;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+Brand brand2 = new Brand() { Name = "asd" };
+Create(brand2);
+
+void Create(Brand brand)
+{
+
+    using (CourseDbContext context = new CourseDbContext())
+    {
+        context.Brands.Add(brand);
+        context.SaveChanges();
+    }
+}
+
+List<Brand> Get(int Id)
+{
+    using(CourseDbContext context = new CourseDbContext())
+    {
+        return context.Brands.Skip(Id).ToList();
+    }
+}
+
+void Delete(int Id)
+{
+
+    using (CourseDbContext context = new CourseDbContext())
+    {
+        var data = context.Brands.Find(Id);
+
+        context.Brands.Remove(data);
+        
+    }
+}
+
+void Update(string name)
+{
+    using (CourseDbContext context = new CourseDbContext())
+    {
+        var data =context.Brands.Find();
+
+        data.Name = name;
+        context.SaveChanges();
+    }
+}
+
+
